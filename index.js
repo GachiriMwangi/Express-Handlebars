7cc6b273288a2dbcbdd18b72e606da468f5e89da
= <=>
2023-08-09 13:17:42 +0300

    Adding a dotenv file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express') 
 const app = express()
 const exp = require('express-handlebars') 
+require('dotenv').config()
 
 app.engine('handlebars', exp.engine())
 app.set('view engine', 'handlebars') 
  
-const PORT = 5000
+const PORT = process.env.PORT
 app.get('/', (req, res) => {
 res.render('home')
 })
